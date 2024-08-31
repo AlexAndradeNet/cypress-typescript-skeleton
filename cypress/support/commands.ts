657a67d45a58557c5b09636389572b372261cc88
Alex Andrade <alexander.andrade@gmail.com>
2024-08-31 18:51:27 -0500

    🔧 improve typescript config

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,6 @@
 /// <reference types="@testing-library/cypress" />
 /// <reference types="cypress" />
+/// <reference types="@badeball/cypress-cucumber-preprocessor" />
 
 import 'cypress-iframe';
 import '@testing-library/cypress/add-commands';
--- scripts/sonar-project.js
+++ scripts/sonar-project.js
@@ -2,20 +2,18 @@
 const scanner = require('sonarqube-scanner').default;
 
 scanner(
-        {
-            serverUrl: 'http://localhost:9000',
-            options: {
-                'sonar.scm.provider': 'git',
-            },
+    {
+        serverUrl: 'http://localhost:9000',
+        options: {
+            'sonar.scm.provider': 'git',
         },
-        error => {
-            if (error) {
-                // eslint-disable-next-line no-console
-                console.error(error);
-            }
-            process.exit();
-        },
+    },
+    error => {
+        if (error) {
+            console.error(error);
+        }
+        process.exit();
+    }
 ).then(() => {
-    // eslint-disable-next-line no-console
     console.log('Sonarqube analysis finished');
 });
